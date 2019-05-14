00658b2a9e297090bd03b9f3fed457bd76af8ca5
kbefff <kbeffel10@me.com>
2019-05-14 13:35:17 -0700

    routing

--- client/src/App.js
+++ client/src/App.js
@@ -2,10 +2,7 @@ import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Link } from "react-router-dom";
 
 
-import CreateChore from "./components/create-chore.component";
-import EditChore from "./components/edit-chore.component";
-import ChoresList from "./components/chores-list.component";
-import MapView from './pages/MapView';
+
 import Navbar from './components/layout/Navbar';
 import Landing from './components/layout/Landing';
 
@@ -18,11 +15,7 @@ class App extends Component {
       <Router>
         <div className="container">
           <Navbar />
-          <Landing />
-          {/* <Route path="/" exact component={ChoresList} />
-          <Route path="/edit/:id" component={EditChore} />
-          <Route path="/create" component={CreateChore} />
-          <Route path="/map" component={MapView}/> */}
+       
         </div>
       </Router>
     );
