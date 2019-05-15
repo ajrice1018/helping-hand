ba0a9d35080faf38c82b4701b650cc15e8777f4d
kbefff <kbeffel10@me.com>
2019-05-15 07:28:14 -0700

    add landing to routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,19 @@
-import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
+import React, {Component} from 'react';
+import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
-
-
-
-import Navbar from './components/layout/Navbar';
-import Landing from './components/layout/Landing';
-
-
+import Navbar from './components/Navbar';
 
 class App extends Component {
-  render() {
-    return (
+    render() {
+        return (
 
-      <Router>
-        <div className="container">
-
-          <Navbar />
-       
-
-        </div>
-      </Router>
-    );
-  }
+            <Router>
+                <div className="container">
+                    <Navbar/>
+                </div>
+            </Router>
+        );
+    }
 }
 
 export default App;
