2f9361ff15bd7de66fd16faade56958e06b4c423
kbefff <kbeffel10@me.com>
2019-05-15 07:31:14 -0700

    App.js cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,8 @@ import React, {Component} from 'react';
 import {BrowserRouter as Router, Route, Link} from "react-router-dom";
 
 import Navbar from './components/Navbar';
+import './App.css';
+
 
 class App extends Component {
     render() {
