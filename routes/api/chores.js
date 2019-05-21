876f936bb0480925816daecc079aac6edb436805
tochwat <tochwat@gmail.com>
2019-05-20 21:28:57 -0700

    Accept button working for requested chores

--- routes/api/chores.js
+++ routes/api/chores.js
@@ -61,6 +61,8 @@ choresRouter.route('/add').post(function(req, res) {
 
 
 choresRouter.route('/update/:id').post(function(req, res) {
+    console.log("route post: ");
+    console.log(req.params.id);
     Chore.findById(req.params.id, function(err, chore) {
         if (!chore)
             res.status(404).send('data is not found');
@@ -69,6 +71,8 @@ choresRouter.route('/update/:id').post(function(req, res) {
             chore.chore_responsible = req.body.chore_responsible;
             chore.chore_completed = req.body.chore_completed;
             chore.chore_address = req.body.chore_address;
+            chore.chore_phone = req.body.chore_phone;
+            chore.chore_status = req.body.chore_status;
             chore.save().then(chore => {
                 res.json('chore updated');
             })
