f699200bbc2df5b34386452a3852d75883baa809
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-05-24 08:27:12 -0700

    Updated server.js file

--- server.js
+++ server.js
@@ -15,9 +15,7 @@ mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useFindAndMod
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-    app.use(express.static("client/build"));
-} 
+
 
 
 
