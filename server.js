037dc2003ffd378f58af22b45711d94d9b3b4704
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-05-24 17:23:21 -0700

    Added server coding to connect to DB

--- server.js
+++ server.js
@@ -15,8 +15,9 @@ mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useFindAndMod
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-
-
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 
 app.get('/', (req, res) => res.send('API Running'));
