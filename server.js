6e3222bd000d62fcdd04761d70b25e14308c2be0
Andrew Rice <andrewrice@Andrews-MacBook.hsd1.wa.comcast.net>
2019-05-27 15:49:24 -0700

    server file updates

--- server.js
+++ server.js
@@ -15,14 +15,9 @@ mongoose.connect(db, {useNewUrlParser: true, useCreateIndex: true, useFindAndMod
 app.use(cors());
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
-if (process.env.NODE_ENV === 'production') {
-    // Serve any static files
-    app.use(express.static(path.join(__dirname, './client/build')));
-  // Handle React routing, return all requests to React app
-    app.get('*', function(req, res) {
-      res.sendFile(path.join(__dirname, './client/build', 'index.html'));
-    });
-  }
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static("client/build"));
+}
 
 
 // app.get('/', (req, res) => res.send('API Running'));
