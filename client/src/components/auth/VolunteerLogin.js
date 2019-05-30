77684983ad831b7aacdb84af5c67f2d61714f1e1
kbefff <kbeffel10@me.com>
2019-05-29 21:00:47 -0700

    really minor styling change

--- client/src/components/auth/VolunteerLogin.js
+++ client/src/components/auth/VolunteerLogin.js
@@ -126,10 +126,10 @@ const VolunteerLogin = ({ loginVolunteer, isAuthenticated }) => {
                     </Button>
                     <Grid container justify="flex-end">
                         <Grid item>
-                            <Link href="#" variant="body2">
+                            <p>
                                 Don't have an account?
                                 <Link to="/volunteer-register"> Sign Up</Link>
-                            </Link>
+                            </p>
                         </Grid>
                     </Grid>
                 </form>
